82a0f2b9ff37baffbb5e1f0db4478d3023d9dfe2
xixinRunBoy <godlikeonline@hotmail.com>
2023-10-19 17:12:09 +0800

    feat: 添加调试信息

--- examples/hello-world/pages/about.tsx
+++ examples/hello-world/pages/about.tsx
@@ -1,3 +1,58 @@
-export default function AboutPage() {
-  return <div>About us</div>
+import React from 'react'
+import { GetServerSideProps } from 'next'
+
+type RepoProps = {
+  name: string
+  description: string
+  subscribers: number
+  stars: number
+  forks: number
 }
+
+const Repo: React.FC<RepoProps> = ({
+  name,
+  subscribers,
+  stars,
+  forks,
+  description,
+}: RepoProps) => {
+  return (
+    <>
+      <h1>Inspecting: {name}</h1>
+      <p>{description}</p>
+      <ul>
+        <li>Subscribers: {subscribers} 👀</li>
+        <li>Stars: {stars} ⭐️</li>
+        <li>Forks: {forks} 🍴</li>
+        <li>Forks: {forks} 🍴</li>
+        <li>Forks: {forks} 🍴123414</li>
+      </ul>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<RepoProps> = async (
+  ctx
+) => {
+  // const { owner, repo } = ctx.query;
+  let owner = 'vercel'
+  let repo = 'next.js'
+  let sss = 'googd3332345565'
+
+  const res = await fetch(`https://api.github.com/repos/${owner}/${repo}`)
+  const data = await res.json()
+
+  const props: RepoProps = {
+    name: data['full_name'] || 'unknown',
+    description: data['description'] || 'unknown',
+    subscribers: data['subscribers_count'] || 'unknown',
+    stars: data['stargazers_count'] || 'unknown',
+    forks: data['forks'] || 'unknown',
+  }
+
+  return {
+    props,
+  }
+}
+
+export default Repo
--- examples/hello-world/pages/index.tsx
+++ examples/hello-world/pages/index.tsx
@@ -2,8 +2,16 @@ import Link from 'next/link'
 
 export default function IndexPage() {
   return (
-    <div>
-      Hello World. <Link href="/about">About</Link>
-    </div>
+    <>
+      <div>
+        Hello World. <Link href="/about">About</Link>
+      </div>
+      {/* <ul>
+        <li><Link href="facebook/react">facebook/react</Link></li>
+        <li><Link href="vercel/next.js">vercel/next.js</Link></li>
+        <li><Link href="framer/motion">framer/motion</Link></li>
+        <li><Link href="tailwindlabs/tailwindcss">tailwindlabs/tailwindcss</Link></li>
+      </ul> */}
+    </>
   )
 }
